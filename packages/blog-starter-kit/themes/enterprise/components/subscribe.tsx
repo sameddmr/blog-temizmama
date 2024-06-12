ed67ffee629b9f5ca3a924e4a43809b486d43411
Bigo <26485488+EXELANCE-LLC@users.noreply.github.com>
2024-06-12 16:32:46 +0300

    Update subscribe.tsx

--- packages/blog-starter-kit/themes/enterprise/components/subscribe.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/subscribe.tsx
@@ -1,10 +1,1 @@
-import * as Popover from '@radix-ui/react-popover';
-import { Button } from './button';
-import { NewsletterPlusSVG } from './icons';
-import { SubscribeForm } from './subscribe-form';
 
-export const Subscribe = () => {
-	return (
-		
-	);
-};
