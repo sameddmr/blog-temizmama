b1f1b4db553fff4077d9cca1af9486d6563b4f99
Bigo <26485488+EXELANCE-LLC@users.noreply.github.com>
2024-06-12 22:47:24 +0300

    Update footer.tsx

--- packages/blog-starter-kit/themes/enterprise/components/footer.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/footer.tsx
@@ -8,6 +8,8 @@ const PUBLICATION_LOGO = publication.preferences.logo;
 return (
 <footer className="border-t py-20 dark:border-neutral-800 ">
   <Container className="px-5">
+     <div className="mx-auto max-w-5xl px-4 py-16 sm:px-6 lg:px-8">
+      <div className="flex justify-center text-teal-600">
     {PUBLICATION_LOGO ? (
     <div className="mb-20 flex w-full flex-row justify-center">
       <Link href={'/'} aria-label={`${publication.title} home page`} className="flex flex-row items-center gap-5">
@@ -19,9 +21,6 @@ return (
       {publication.title}
     </p>
     )}
-     <div className="mx-auto max-w-5xl px-4 py-16 sm:px-6 lg:px-8">
-      <div className="flex justify-center text-teal-600">
-
       </div>
 
       <p className="mx-auto mt-6 max-w-md text-center leading-relaxed text-gray-500">
