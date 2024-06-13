0059e70bfd282ea63d010e18ed12a007181baf67
Bigo <26485488+EXELANCE-LLC@users.noreply.github.com>
2024-06-13 10:36:09 +0300

    author gibi bilgiler silindi ve subscribe silindi

--- packages/blog-starter-kit/themes/enterprise/pages/[slug].tsx
+++ packages/blog-starter-kit/themes/enterprise/pages/[slug].tsx
@@ -29,11 +29,11 @@ import { useEffect, useState } from 'react';
 // @ts-ignore
 import { triggerCustomWidgetEmbed } from '@starter-kit/utils/trigger-custom-widget-embed';
 
-const AboutAuthor = dynamic(() => import('../components/about-author'), { ssr: false });
-const Subscribe = dynamic(() => import('../components/subscribe').then((mod) => mod.Subscribe));
-const PostComments = dynamic(() =>
-	import('../components/post-comments').then((mod) => mod.PostComments),
-);
+// const AboutAuthor = dynamic(() => import('../components/about-author'), { ssr: false });
+// const Subscribe = dynamic(() => import('../components/subscribe').then((mod) => mod.Subscribe));
+// const PostComments = dynamic(() =>
+//	import('../components/post-comments').then((mod) => mod.PostComments),
+// );
 
 type PostProps = {
 	type: 'post';
@@ -129,9 +129,6 @@ const Post = ({ publication, post }: PostProps) => {
 			<PostHeader
 				title={post.title}
 				coverImage={post.coverImage?.url}
-				date={post.publishedAt}
-				author={post.author}
-				readTimeInMinutes={post.readTimeInMinutes}
 			/>
 			{post.features.tableOfContents.isEnabled && <PostTOC />}
 			<MarkdownToHtml contentMarkdown={post.content.markdown} />
@@ -140,9 +137,6 @@ const Post = ({ publication, post }: PostProps) => {
 					<ul className="flex flex-row flex-wrap items-center gap-2">{tagsList}</ul>
 				</div>
 			)}
-			<AboutAuthor />
-			{!post.preferences.disableComments && post.comments.totalDocuments > 0 && <PostComments />}
-			<Subscribe />
 		</>
 	);
 };
