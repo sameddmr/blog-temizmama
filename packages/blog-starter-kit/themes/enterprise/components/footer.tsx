8dd4ce8eace099533898d7c7b5ef4495757ff1b3
Bigo <26485488+EXELANCE-LLC@users.noreply.github.com>
2024-06-20 10:47:35 +0300

    çağatay url eklendi

--- packages/blog-starter-kit/themes/enterprise/components/footer.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/footer.tsx
@@ -11,12 +11,15 @@ return (
      <div className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
       <div className="flex justify-center text-teal-600">
     {PUBLICATION_LOGO ? (
-    <div className="flex w-full flex-row justify-center">
-      <Link href={'/'} aria-label={`${publication.title} home page`} className="flex flex-row items-center gap-5">
-      <img className="block w-40" src={PUBLICATION_LOGO} alt={publication.title} />
-        <img className="block w-40" src="https://www.temizmama.biz/dist/media/img/markalar/bg-cgty.webp" alt={publication.title} />
-      </Link>
-    </div>
+    <><div className="flex w-full flex-row justify-center">
+              <Link href={'/'} aria-label={`${publication.title} home page`} className="flex flex-row items-center gap-5">
+                <img className="block w-40" src={PUBLICATION_LOGO} alt={publication.title} />
+              </Link>
+            </div><div className="flex w-full flex-row justify-center">
+                <Link href={'https://www.cagatay.com'} aria-label={`${publication.title} home page`} className="flex flex-row items-center gap-5">
+                  <img className="block w-40" src="https://www.temizmama.biz/dist/media/img/markalar/bg-cgty.webp" alt={publication.title} />
+                </Link>
+              </div></>
     ) : (
     <p className="text-center text-xl font-semibold text-slate-900 dark:text-slate-50 md:text-4xl">
       {publication.title}
